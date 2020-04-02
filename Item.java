6f462c023fc5e0fbaa52df33c1be4d83e31a14da
Mihajlo Kragujevski <mihajlo.kragujevski@yahoo.com>
2020-04-02 22:31:37 +0200

    tax type variable added

--- Item.java
+++ Item.java
@@ -4,6 +4,20 @@ class Item {
 	double price;
 
 	//TODO add variable.
+	class Tip{
+		static double a = 0.18, b = 0.5, c = 0.0;
+
+		double geta(){
+			return a;
+		}
+		double getb(){
+			return b;
+		}
+		double getc(){
+			return c;
+		}
+	}
+	Tip tax = new Tip();
 
 	//TODO constructor
 
